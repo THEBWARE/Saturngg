9f97ed97358982e994bb65fe009b3a9783db1725
THEB <Worldroleplayman@gmail.com>
2025-04-14 21:19:46 -0400

    Update Bootstrapper.py

--- Bootstrapper.py
+++ Bootstrapper.py
@@ -2,19 +2,18 @@ import requests
 import os
 import zipfile
 
-def display_ascii_art():  # Note: Two 'i's in "ascii"
+def display_ascii_art():
     # ANSI escape code for blue text
     blue_color = "\033[94m"
     # ANSI escape code to reset text color to default
     reset_color = "\033[0m"
 
     ascii_art = r"""
-  ____        _                                 
- / ___|  __ _| |_ _   _ _ __ _ __    __ _  __ _ 
- \___ \ / _` | __| | | | '__| '_ \  / _` |/ _` |
-  ___) | (_| | |_| |_| | |  | | | || (_| | (_| |
- |____/ \__,_|\__|\__,_|_|  |_| |_(_)__, |\__, |
-                                    |___/ |___/ 
+____  ____  _   _ __  ____        ___    ____  _____ 
+|  _ \|  _ \| | | |  \/  \ \      / / \  |  _ \| ____|
+| | | | |_) | | | | |\/| |\ \ /\ / / _ \ | |_) |  _|  
+| |_| |  _ <| |_| | |  | | \ V  V / ___ \|  _ <| |___ 
+|____/|_| \_\\___/|_|  |_|  \_/\_/_/   \_\_| \_\_____|
     """
     print(f"{blue_color}{ascii_art}{reset_color}")
 
@@ -23,7 +22,7 @@ def download_file(url, filename):
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
         with open(filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192): 
+            for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
     print(f"Step 2: {filename} downloaded successfully.")
 
@@ -34,11 +33,12 @@ def unzip_file(filename, extract_to):
     print(f"Step 4: {filename} unzipped successfully.")
 
 def main():
-    display_ascii_art()  # Must match the defined name exactly
-    url = "https://github.com/THEBWARE/Saturngg/releases/download/Source/Saturn.zip"
-    zip_filename = "Saturn.zip"
-    extract_to = "Saturn"
+    display_ascii_art()
+    url = "https://github.com/THEBWARE/DRUMWARE/releases/download/DRUMWARE/DRUMWARE-V1.1.9.zip"
+    zip_filename = "DRUMWARE-V1.1.9.zip"
+    extract_to = "DRUMWARE-V1.1.9"
 
+    # Ensure the directory exists
     if not os.path.exists(extract_to):
         os.makedirs(extract_to)
 
